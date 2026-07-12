dc21138e30aab3e98a1a9fe6ce7be0267159817a
Donia Shaban <doniashaban723@gmail.com>
2026-07-12 11:04:40 +0300

    feat(infrastructure): implement repository and update DbContext

--- Restaurant.Infrastructure/Repositories/RestaurantRepository.cs
+++ Restaurant.Infrastructure/Repositories/RestaurantRepository.cs
@@ -1,14 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Restaurant.Application.Common.Interfaces.Repositories;
+﻿using Restaurant.Application.Common.Interfaces.Repositories;
+using Restaurant.Infrastructure.Data;
 
+namespace Restaurant.Infrastructure.Repositories;
 
-namespace Restaurant.Infrastructure.Repositories
+public sealed class RestaurantRepository(RestaurantDbContext context)
+    : IRestaurantRepository
 {
-    public class RestaurantRepository : IRestaurantRepository
+    private readonly RestaurantDbContext _context = context;
+
+    public async Task AddAsync(
+        Domain.Restaurants.Restaurant restaurant,
+        CancellationToken cancellationToken = default)
     {
+        await _context.Restaurants.AddAsync(
+            restaurant,
+            cancellationToken);
+    }
+
+    public async Task SaveChangesAsync(
+        CancellationToken cancellationToken = default)
+    {
+        await _context.SaveChangesAsync(cancellationToken);
     }
 }

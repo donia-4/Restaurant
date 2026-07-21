d50e438df3ac91a323893da338212d49b4a588b8
Donia Shaban <doniashaban723@gmail.com>
2026-07-21 14:43:43 +0300

    feat: add RabbitMQ infrastructure

--- Restaurant.Infrastructure/RabbitMQ/RabbitMqOptions.cs
+++ Restaurant.Infrastructure/RabbitMQ/RabbitMqOptions.cs
@@ -1,12 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Restaurant.Application.Common.Messages;
 
-namespace Restaurant.Infrastructure.RabbitMQ
+namespace Restaurant.Infrastructure.RabbitMQ;
+
+public sealed class RabbitMqOptions
 {
-    internal class RabbitMqOptions
-    {
-    }
+    public const string SectionName = "RabbitMQ";
+
+    public string HostName { get; init; } = default!;
+
+    public int Port { get; init; } = 5672;
+
+    public string UserName { get; init; } = default!;
+
+    public string Password { get; init; } = default!;
+
+    public string VirtualHost { get; init; } = "/";
+
+    public string ExchangeName { get; init; } = ExchangeNames.RestaurantEvents;
 }

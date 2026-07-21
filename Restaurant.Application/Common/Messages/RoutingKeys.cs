9cdb447c3e438abbe5d1257524abb0c51489e870
Donia Shaban <doniashaban723@gmail.com>
2026-07-21 14:38:21 +0300

    feat: add messaging abstractions and routing keys

---
+++ Restaurant.Application/Common/Messages/RoutingKeys.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant.Application.Common.Messages
+{
+    public static class RoutingKeys
+    {
+        public const string RestaurantRequested =  
+        "restaurant.requested";
+
+        public const string RestaurantApproved =
+            "restaurant.approved";
+
+        public const string RestaurantRejected =
+            "restaurant.rejected";
+
+        public const string RestaurantStatusChanged =
+            "restaurant.status.changed";
+    }
+}

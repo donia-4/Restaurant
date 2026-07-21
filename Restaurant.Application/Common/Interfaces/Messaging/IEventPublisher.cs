a0a5a6bc64007b0532be73096181bad11b15519b
Donia Shaban <doniashaban723@gmail.com>
2026-07-21 14:43:08 +0300

    feat: add messaging abstractions and routing keys

--- Restaurant.Application/Common/Interfaces/Messaging/IEventPublisher.cs
+++ Restaurant.Application/Common/Interfaces/Messaging/IEventPublisher.cs
@@ -4,12 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Restaurant.Application.Common.Interfaces
+namespace Restaurant.Application.Common.Interfaces.Messaging
 {
     public interface IEventPublisher
     {
-        Task PublishAsync<T>(
-            T integrationEvent,
-            CancellationToken cancellationToken = default);
+        Task PublishAsync<TEvent>(
+            TEvent @event,
+            string routingKey,
+            CancellationToken cancellationToken = default)
+            where TEvent : class;
     }
 }
--- Restaurant.Application/Common/Messages/ExchangeNames.cs
+++ Restaurant.Application/Common/Messages/ExchangeNames.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace Restaurant.Application.Common.Messages
 {
-    internal class ExchangeNames
+    public static class ExchangeNames
     {
+        public const string RestaurantEvents = "restaurant.events";
+
+        public const string OrderEvents = "order.events";
+
+        public const string NotificationEvents = "notification.events";
     }
 }
